910d2a8df53c1cc8d209b3777d2dc77a800b05a4
rishiyadav11 <photonics54yadav@gmail.com>
2025-05-17 20:01:54 +0530

    initialise an app

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ if ('serviceWorker' in navigator) {
       .catch(err => console.log('Service Worker registration failed', err));
   });
 }
+
+
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+
+  const installBtn = document.getElementById("install");
+  installBtn.style.display = "inline";
+
+  installBtn.addEventListener("click", () => {
+    installBtn.style.display = "none";
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the install prompt");
+      } else {
+        console.log("User dismissed the install prompt");
+      }
+      deferredPrompt = null;
+    });
+  });
+});
